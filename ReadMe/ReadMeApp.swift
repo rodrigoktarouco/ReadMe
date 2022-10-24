2701cd296aa9f8485aaca60880cb933b0195ab6c
Rodrigo Kroef tarouco <rodrigoktarouco@gmail.com>
2022-10-24 09:07:08 -0300

    First commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Rodrigo Kroef Tarouco on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
